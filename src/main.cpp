e6333904cbb13aff3567af0d1e06eada00fe5b4f
Arnav Kohli <arnavkohli2003@gmail.com>
2024-03-10 12:22:21 +0000

    Added Mengjie's setRow, everything fine so far
    Signed-off-by: Arnav Kohli <arnavkohli2003@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -36,6 +36,24 @@ U8G2_SSD1305_128X32_NONAME_F_HW_I2C u8g2(U8G2_R0);
 const GPIO_PinState gpio_state[2] = {GPIO_PIN_RESET, GPIO_PIN_SET};
 // This stores the gpio pinouts corresponding to column idx C0 - 3
 const uint32_t key_cols[4] = {GPIO_PIN_3, GPIO_PIN_8, GPIO_PIN_7, GPIO_PIN_9};
+//// Additional variables
+std::string key;
+int keynums;
+int tone_idx[6] = {0}; // The indices (0 - 12) of the waveform periods that the are being selected to play in the audio
+std::vector<std::vector<std::vector<uint32_t>>> waveform_luts; // the waveform lut stores the insturment waveforms.
+int nok = 0; // No. of keys currently being presses simutaneously
+int instru = 0; // Instrument idx, current there are 4 instruments
+// Recodes whether a key is being presses. This filters out the pressed keys and enables the key to be detected in the order of their presses
+bool press_list[12] = {false};
+
+const uint32_t frequencies[12] = {262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494};
+std::string keystrings[12] = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+uint32_t Ts[13] = {};
+
+int t = 0; // Initialised timer
+float decay[6] = {1};
+float decay_factor = 0.99999;
+uint32_t Vout;
 
 //Function to set outputs using key matrix
 void setOutMuxBit(const uint8_t bitIdx, const bool value) {
@@ -64,15 +82,15 @@ std::bitset<4> readCols(){
   return result;
 }
 
-void setRow(uint8_t rowIdx){
-  digitalWrite(REN_PIN, LOW);
-  std::bitset<3> rowBits = std::bitset<3>(rowIdx);
-
-  digitalWrite(RA0_PIN, rowBits[0]);
-  digitalWrite(RA1_PIN, rowBits[1]);
-  digitalWrite(RA2_PIN, rowBits[2]);
-
-  digitalWrite(REN_PIN, HIGH);
+void setRow(uint8_t rowidx){
+	std::bitset<3> Ridx = std::bitset<3>(rowidx);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET); // REN_PIN
+	delayMicroseconds(2);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, gpio_state[Ridx[0]]); // RA0_PIN
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, gpio_state[Ridx[1]]); // RA1_PIN
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, gpio_state[Ridx[2]]); // RA2_PIN
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET); // REN_PIN
+	delayMicroseconds(2);
 }
 
 void sampleISR(){
