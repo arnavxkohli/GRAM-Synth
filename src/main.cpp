f63452fd410bb168a7c7cb300c141d73c8dbb739
Jerry7234234 <134069780+Jerry7234234@users.noreply.github.com>
2024-03-11 19:17:53 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -80,7 +80,7 @@ void setRow(uint8_t rowidx){
 	delayMicroseconds(2);
 }
 
-void sampleISR(){
+void sampleISR() {
   uint32_t localRotation = volumeKnob.getRotationISR();
 	// Serial.print(nok);
 	// If there's at least a key being presses, do something
@@ -93,11 +93,11 @@ void sampleISR(){
 		// Ts = the 13 periods of the keys, the first period is 1 corresponding to no keys
 		// instru = Selects the instrument, currrent is 0 - 3
 		Vout = (waveform_luts[instru][tone_idx[0]][(t % Ts[tone_idx[0]])] * decay[0] +
-					  waveform_luts[instru][tone_idx[1]][(t % Ts[tone_idx[1]])] * decay[1] +
-					  waveform_luts[instru][tone_idx[2]][(t % Ts[tone_idx[2]])] * decay[2] +
-					  waveform_luts[instru][tone_idx[3]][(t % Ts[tone_idx[3]])] * decay[3] +
-					  waveform_luts[instru][tone_idx[4]][(t % Ts[tone_idx[4]])] * decay[4] +
-					  waveform_luts[instru][tone_idx[5]][(t % Ts[tone_idx[5]])] * decay[5]) / std::max(nok, 1); // Divide the amplitude by the totoal number of keys being pressed
+ 					waveform_luts[instru][tone_idx[1]][(t % Ts[tone_idx[1]])] * decay[1] +
+				 	waveform_luts[instru][tone_idx[2]][(t % Ts[tone_idx[2]])] * decay[2] +
+					waveform_luts[instru][tone_idx[3]][(t % Ts[tone_idx[3]])] * decay[3] +
+					waveform_luts[instru][tone_idx[4]][(t % Ts[tone_idx[4]])] * decay[4] +
+					waveform_luts[instru][tone_idx[5]][(t % Ts[tone_idx[5]])] * decay[5]) / std::max(nok, 1); // Divide the amplitude by the totoal number of keys being pressed
     // Vout = Vout >> (12 - localRotation);
     // Sets audio resolution to 12
     analogWriteResolution(12);
@@ -134,39 +134,31 @@ void sampleISR(){
 
 
 void CAN_RX_ISR (void) {
-  uint8_t RX_Message_ISR[8];
-  uint32_t ID;
-  CAN_RX(ID, RX_Message_ISR);
-  xQueueSendFromISR(msgInQ, RX_Message_ISR, NULL);
+	uint8_t RX_Message_ISR[8];
+	uint32_t ID;
+	CAN_RX(ID, RX_Message_ISR);
+	xQueueSendFromISR(msgInQ, RX_Message_ISR, NULL);
 }
 
 void CAN_TX_ISR (void) {
-  xSemaphoreGiveFromISR(CAN_TX_Semaphore, NULL);
+	xSemaphoreGiveFromISR(CAN_TX_Semaphore, NULL);
 }
 
-void scanKeysTask(void * pvParameters) {
-  const TickType_t xFrequency = 20/portTICK_PERIOD_MS;
-  TickType_t xLastWakeTime = xTaskGetTickCount();
-  uint8_t TX_Message[8] = {0};
-  TX_Message[1] = 4; // Octave to be changed later - try auto assigning this.
+void scanKeysFunction(uint8_t* TX_Message) {
+	// uint32_t localCurrentStepSize = 0;
 
-  while(1){
-    vTaskDelayUntil( &xLastWakeTime, xFrequency );
+	// std::bitset<32> currentInputs = inputs.getCurrentInputs();
+	// std::bitset<32> previousInputs = inputs.getPreviousInputs();
 
-    // uint32_t localCurrentStepSize = 0;
+	// for(int row = 0; row < 4; row++){
+	//   setRow(row);
+	//   delayMicroseconds(3);
+	//   for (int bit = 0; bit < 4; ++bit) {
+	//       currentInputs[row * 4 + bit] = readCols()[bit];
+	//   }
+	// }
 
-    // std::bitset<32> currentInputs = inputs.getCurrentInputs();
-    // std::bitset<32> previousInputs = inputs.getPreviousInputs();
-
-    // for(int row = 0; row < 4; row++){
-    //   setRow(row);
-    //   delayMicroseconds(3);
-    //   for (int bit = 0; bit < 4; ++bit) {
-    //       currentInputs[row * 4 + bit] = readCols()[bit];
-    //   }
-    // }
-
-    for (int i = 0; i < 12; i++) {
+	for (int i = 0; i < 12; i++) {
 			setRow(i / 4); // Floor division
 			delayMicroseconds(3);
 			// xSemaphoreTake(sysState.mutex, portMAX_DELAY);
@@ -178,7 +170,7 @@ void scanKeysTask(void * pvParameters) {
 				press_list[i] = true; // Set the "is-pressed" entry for that key to true
 				tone_idx[nok] = i + 1;
 				TX_Message[0] = 'P';
-        TX_Message[2] = i; // Assign number of note played to 3rd entry in transmission
+		TX_Message[2] = i; // Assign number of note played to 3rd entry in transmission
 				key = key + keystrings[i];
 				nok ++; // Increase the no. of key being presses
 			}
@@ -199,189 +191,252 @@ void scanKeysTask(void * pvParameters) {
 		setRow(3); // Select the knob row
 		delayMicroseconds(3);
 
-    volumeKnob.updateRotation(std::to_string(!HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_8)) +
-      std::to_string(!HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_3)));
-    decayKnob.updateRotation(std::to_string(!HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_9)) +
-      std::to_string(!HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_7)));
+	volumeKnob.updateRotation(std::to_string(!HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_8)) +
+		std::to_string(!HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_3)));
+	decayKnob.updateRotation(std::to_string(!HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_9)) +
+		std::to_string(!HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_7)));
 
-    setRow(4);
-    delayMicroseconds(3);
-    instrumentKnob.updateRotation(std::to_string(!HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_8)) +
-      std::to_string(!HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_3)));
+	setRow(4);
+	delayMicroseconds(3);
+	instrumentKnob.updateRotation(std::to_string(!HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_8)) +
+		std::to_string(!HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_3)));
 
-    // for(int i = 0; i < 12; i++){
-    //   if(currentInputs[i]){
-    //     localCurrentStepSize = stepSizes[i];
-    //     TX_Message[2] = i;
-    //     if(!previousInputs[i]){
-    //       TX_Message[0] = 'P';
-    //       __atomic_store_n(&currentStepSize, localCurrentStepSize, __ATOMIC_RELAXED);
-    //     }
-    //   }
-    //   else if (previousInputs[i]) {
-    //     TX_Message[0] = 'R';
-    //   }
-    // }
+	// for(int i = 0; i < 12; i++){
+	//   if(currentInputs[i]){
+	//     localCurrentStepSize = stepSizes[i];
+	//     TX_Message[2] = i;
+	//     if(!previousInputs[i]){
+	//       TX_Message[0] = 'P';
+	//       __atomic_store_n(&currentStepSize, localCurrentStepSize, __ATOMIC_RELAXED);
+	//     }
+	//   }
+	//   else if (previousInputs[i]) {
+	//     TX_Message[0] = 'R';
+	//   }
+	// }
 
-    xQueueSend(msgOutQ, TX_Message, portMAX_DELAY);
-  }
+	xQueueSend(msgOutQ, TX_Message, portMAX_DELAY);
+}
+
+void displayUpdateFunction(uint32_t ID, uint8_t* localRX) {
+	//Update display
+	u8g2.clearBuffer();         // clear the internal memory
+
+	u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
+	u8g2.drawStr(2, 10, "Music Synth");  // write something to the internal memory
+	u8g2.setCursor(2, 20);
+
+	u8g2.print(volumeKnob.getRotation());
+
+	// xSemaphoreTake(sysState.mutex, portMAX_DELAY);
+	// u8g2.drawStr(2, 30, sysState.notePlayed);
+	// xSemaphoreGive(sysState.mutex);
+	memcpy(localRX, rxMessage.getRX_Message(), 8);
+	u8g2.setCursor(66,30);
+	u8g2.print((char) localRX[0]);
+	u8g2.print(localRX[1]);
+	u8g2.print(localRX[2]);
+
+	u8g2.sendBuffer();          // transfer internal memory to the display
+
+	//Toggle LED
+	digitalToggle(LED_BUILTIN);
+}
+
+void CAN_RX_Function(uint8_t* local_RX) {
+		xQueueReceive(msgInQ, local_RX, portMAX_DELAY);
+		rxMessage.receiveMessage(local_RX);
+		__atomic_store_n(&currentStepSize, rxMessage.getStepSize(), __ATOMIC_RELAXED);
+}
+
+void CAN_TX_Function(uint8_t* msgOut) {
+	xQueueReceive(msgOutQ, msgOut, portMAX_DELAY);
+	xSemaphoreTake(CAN_TX_Semaphore, portMAX_DELAY);
+	CAN_TX(0x123, msgOut);
+	//xSemaphoreGive(CAN_TX_Semaphore);
+}
+
+void scanKeysTask(void * pvParameters) {
+	const TickType_t xFrequency = 20/portTICK_PERIOD_MS;
+	TickType_t xLastWakeTime = xTaskGetTickCount();
+	uint8_t TX_Message[8] = {0};
+	TX_Message[1] = 4; // Octave to be changed later - try auto assigning this.
+
+	while(1) {
+		vTaskDelayUntil( &xLastWakeTime, xFrequency );
+		scanKeysFunction(TX_Message);
+	}
 }
 
 void displayUpdateTask(void * pvParameters){
-  const TickType_t xFrequency = 100/portTICK_PERIOD_MS;
-  TickType_t xLastWakeTime = xTaskGetTickCount();
-  uint32_t ID;
-  uint8_t localRX[8];
+	const TickType_t xFrequency = 100/portTICK_PERIOD_MS;
+	TickType_t xLastWakeTime = xTaskGetTickCount();
+	uint32_t ID;
+	uint8_t localRX[8];
 
-  while(1){
-    vTaskDelayUntil( &xLastWakeTime, xFrequency );
-
-    //Update display
-    u8g2.clearBuffer();         // clear the internal memory
-
-    u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
-    u8g2.drawStr(2, 10, "Music Synth");  // write something to the internal memory
-    u8g2.setCursor(2, 20);
-
-    u8g2.print(volumeKnob.getRotation());
-
-    // xSemaphoreTake(sysState.mutex, portMAX_DELAY);
-    // u8g2.drawStr(2, 30, sysState.notePlayed);
-    // xSemaphoreGive(sysState.mutex);
-    memcpy(localRX, rxMessage.getRX_Message(), 8);
-    u8g2.setCursor(66,30);
-    u8g2.print((char) localRX[0]);
-    u8g2.print(localRX[1]);
-    u8g2.print(localRX[2]);
-
-    u8g2.sendBuffer();          // transfer internal memory to the display
-
-    //Toggle LED
-    digitalToggle(LED_BUILTIN);
-  }
+	while(1){
+		vTaskDelayUntil( &xLastWakeTime, xFrequency );
+		displayUpdateFunction(ID, localRX);
+	}
 }
 
 void CAN_RX_Task(void* pvParameters){
-  uint8_t local_RX[8];
+	uint8_t local_RX[8];
 
-  while(1){
-    xQueueReceive(msgInQ, local_RX, portMAX_DELAY);
-    rxMessage.receiveMessage(local_RX);
-    __atomic_store_n(&currentStepSize, rxMessage.getStepSize(), __ATOMIC_RELAXED);
-  }
+	while(1) {
+		CAN_RX_Function(local_RX);
+	}
 }
 
 void CAN_TX_Task (void * pvParameters) {
-  uint8_t msgOut[8];
+	uint8_t msgOut[8];
 
-  while (1) {
-    xQueueReceive(msgOutQ, msgOut, portMAX_DELAY);
-    xSemaphoreTake(CAN_TX_Semaphore, portMAX_DELAY);
-    CAN_TX(0x123, msgOut);
-    //xSemaphoreGive(CAN_TX_Semaphore);
-  }
+	while (1) {
+		CAN_TX_Function(msgOut);
+	}
 }
 
 void setup() {
-  // put your setup code here, to run once:
+	//Set pin directions
+	pinMode(RA0_PIN, OUTPUT);
+	pinMode(RA1_PIN, OUTPUT);
+	pinMode(RA2_PIN, OUTPUT);
+	pinMode(REN_PIN, OUTPUT);
+	pinMode(OUT_PIN, OUTPUT);
+	pinMode(OUTL_PIN, OUTPUT);
+	pinMode(OUTR_PIN, OUTPUT);
+	pinMode(LED_BUILTIN, OUTPUT);
 
-  //Set pin directions
-  pinMode(RA0_PIN, OUTPUT);
-  pinMode(RA1_PIN, OUTPUT);
-  pinMode(RA2_PIN, OUTPUT);
-  pinMode(REN_PIN, OUTPUT);
-  pinMode(OUT_PIN, OUTPUT);
-  pinMode(OUTL_PIN, OUTPUT);
-  pinMode(OUTR_PIN, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
+	pinMode(C0_PIN, INPUT);
+	pinMode(C1_PIN, INPUT);
+	pinMode(C2_PIN, INPUT);
+	pinMode(C3_PIN, INPUT);
+	pinMode(JOYX_PIN, INPUT);
+	pinMode(JOYY_PIN, INPUT);
 
-  pinMode(C0_PIN, INPUT);
-  pinMode(C1_PIN, INPUT);
-  pinMode(C2_PIN, INPUT);
-  pinMode(C3_PIN, INPUT);
-  pinMode(JOYX_PIN, INPUT);
-  pinMode(JOYY_PIN, INPUT);
+	//Initialise display
+	setOutMuxBit(DRST_BIT, LOW);  //Assert display logic reset
+	delayMicroseconds(2);
+	setOutMuxBit(DRST_BIT, HIGH);  //Release display logic reset
+	u8g2.begin();
+	setOutMuxBit(DEN_BIT, HIGH);  //Enable display power supply
 
-  //Initialise display
-  setOutMuxBit(DRST_BIT, LOW);  //Assert display logic reset
-  delayMicroseconds(2);
-  setOutMuxBit(DRST_BIT, HIGH);  //Release display logic reset
-  u8g2.begin();
-  setOutMuxBit(DEN_BIT, HIGH);  //Enable display power supply
-
-  waveform_luts.push_back(sawtooth1());
+	waveform_luts.push_back(sawtooth1());
 	waveform_luts.push_back(sinewave());
 	waveform_luts.push_back(piano1());
 	waveform_luts.push_back(sawtooth2());
 
-  Ts[0] = 1;
+	Ts[0] = 1;
 	for (int i = 1; i <= 12; i++) {
 		Ts[i] = 22000 / frequencies[i - 1];
 	}
 
-  //Initialise UART
-  Serial.begin(9600);
+	//Initialise UART
+	Serial.begin(9600);
 
-  msgInQ = xQueueCreate(36,8);
-  msgOutQ = xQueueCreate(36,8);
+	msgInQ = xQueueCreate(36,8);
+	msgOutQ = xQueueCreate(36,8);
 
-  TIM_TypeDef *Instance = TIM1;
-  HardwareTimer *sampleTimer = new HardwareTimer(Instance);
+	TIM_TypeDef *Instance = TIM1;
+	HardwareTimer *sampleTimer = new HardwareTimer(Instance);
 
-  sampleTimer->setOverflow(22000, HERTZ_FORMAT);
-  sampleTimer->attachInterrupt(sampleISR);
-  sampleTimer->resume();
+	sampleTimer->setOverflow(22000, HERTZ_FORMAT);
+	sampleTimer->attachInterrupt(sampleISR);
+	sampleTimer->resume();
 
-  CAN_Init(true);
-  setCANFilter(0x123,0x7ff);
-  CAN_RegisterRX_ISR(CAN_RX_ISR);
-  CAN_RegisterTX_ISR(CAN_TX_ISR);
-  CAN_Start();
+	CAN_Init(true);
+	setCANFilter(0x123,0x7ff);
+	CAN_RegisterRX_ISR(CAN_RX_ISR);
+	CAN_RegisterTX_ISR(CAN_TX_ISR);
+	CAN_Start();
 
-  TaskHandle_t scanKeysHandle = NULL;
-  TaskHandle_t displayUpdateHandle = NULL;
-  TaskHandle_t CAN_RXHandle = NULL;
-  TaskHandle_t CAN_TXHandle = NULL;
+	TaskHandle_t scanKeysHandle = NULL;
+	TaskHandle_t displayUpdateHandle = NULL;
+	TaskHandle_t CAN_RXHandle = NULL;
+	TaskHandle_t CAN_TXHandle = NULL;
 
-  xTaskCreate(
-    displayUpdateTask, /* Function that implements the task */
-    "displayUpdate", /* Text name for the task */
-    256, /* Stack size in words, not bytes */
-    NULL, /* Parameter passed into the task */
-    1, /* Task priority */
-    &displayUpdateHandle /* Pointer to store the task handle */
-  );
+	#ifndef TEST_SCANKEYS
+		xTaskCreate(
+			displayUpdateTask, /* Function that implements the task */
+			"displayUpdate", /* Text name for the task */
+			256, /* Stack size in words, not bytes */
+			NULL, /* Parameter passed into the task */
+			1, /* Task priority */
+			&displayUpdateHandle /* Pointer to store the task handle */
+		);
 
-  xTaskCreate(
-    scanKeysTask,		/* Function that implements the task */
-    "scanKeys",		/* Text name for the task */
-    256,      		/* Stack size in words, not bytes */
-    NULL,			/* Parameter passed into the task */
-    1,			/* Task priority */
-    &scanKeysHandle /* Pointer to store the task handle */
-  );
+		xTaskCreate(
+			scanKeysTask,		/* Function that implements the task */
+			"scanKeys",		/* Text name for the task */
+			256,      		/* Stack size in words, not bytes */
+			NULL,			/* Parameter passed into the task */
+			1,			/* Task priority */
+			&scanKeysHandle /* Pointer to store the task handle */
+		);
 
-  xTaskCreate(
-    CAN_RX_Task,
-    "CAN_RX_Task",
-    64,
-    NULL,
-    1,
-    &CAN_RXHandle
-  );
+		xTaskCreate(
+			CAN_RX_Task,
+			"CAN_RX_Task",
+			64,
+			NULL,
+			1,
+			&CAN_RXHandle
+		);
 
-  xTaskCreate(
-    CAN_TX_Task,
-    "CAN_TX_Task",
-    64,
-    NULL,
-    1,
-    &CAN_TXHandle
-  );
+		xTaskCreate(
+			CAN_TX_Task,
+			"CAN_TX_Task",
+			64,
+			NULL,
+			1,
+			&CAN_TXHandle
+		);
+	#endif
 
-  CAN_TX_Semaphore = xSemaphoreCreateCounting(3,3);
+	#ifdef TEST_SCANKEYS
+		uint8_t TX_Message[8] = {0};
+		uint32_t startTime = micros();
+		for (int iter = 0; iter < 32; iter++) {
+			scanKeysFunction(TX_Message);
+		}
+		std::cout << "Key scanning task latent execution time: " << (micros()-startTime) / 32 << " microseconds" << std::endl;
 
-  vTaskStartScheduler();
+		uint32_t ID;
+		uint8_t localRX[8];
+		startTime = micros();
+		for (int iter = 0; iter < 32; iter++) {
+			displayUpdateFunction(ID, localRX);
+		}
+		std::cout << "Display update task latent execution time: " << (micros()-startTime) / 32 << " microseconds" << std::endl;
+
+		// uint8_t msgOut[8];
+		// startTime = micros();
+		// for (int iter = 0; iter < 1; iter++) {
+		// 	CAN_TX_Function(msgOut);
+		// }
+		// std::cout << "Data transmission task latent execution time: " << (micros()-startTime) / 1 << " microseconds" << std::endl;
+
+		// uint8_t local_RX[8];
+		// startTime = micros();
+		// for (int iter = 0; iter < 1; iter++) {
+		// 	CAN_RX_Function(local_RX);
+		// }
+		// std::cout << "Data receiving task latent execution time: " << (micros()-startTime) / 1 << " microseconds" << std::endl;
+
+		nok = 1;
+		startTime = micros();
+		for (int iter = 0; iter < 32; iter++) {
+			sampleISR();
+		}
+		std::cout << "DAC ISR task latent execution time: " << (micros()-startTime) / 32 << " microseconds" << std::endl;
+
+		std::cout << "" << std::endl;
+		while(1);
+	#endif
+
+	CAN_TX_Semaphore = xSemaphoreCreateCounting(3,3);
+
+	vTaskStartScheduler();
 }
 
 void loop() {
