99d331ff9cb305efe6ddde30a43bc5e76b8b11e9
Jerry7234234 <134069780+Jerry7234234@users.noreply.github.com>
2024-03-17 17:08:37 +0000

    Update waveform.h

--- include/waveform.h
+++ include/waveform.h
@@ -5,16 +5,16 @@
 #include <vector>
 #include <fstream>
 
-std::vector<std::vector<uint32_t>> sinewave() {
-    const double half_range = 2047.25; // Half of the maximum value of uint32_t
-    const uint32_t frequencies[12] = {262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494};
-    std::vector<std::vector<uint32_t>> sinewave_lut;
-    std::vector<uint32_t> zero_vec;
+std::vector<std::vector<uint16_t>> sinewave() {
+    const double half_range = pow(2, 11); // Half of the maximum value of uint16_t
+    const uint16_t frequencies[36] = {130, 138, 147, 155, 165, 174, 185, 196, 207, 220, 233, 247, 262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523, 554, 587, 622, 659, 698, 740, 784, 830, 880, 932, 988};
+    std::vector<std::vector<uint16_t>> sinewave_lut;
+    std::vector<uint16_t> zero_vec;
     zero_vec.push_back(0);
     sinewave_lut.push_back(zero_vec);
-    for (int i = 0; i < 12; i++) {
-        uint32_t T = 22000 / frequencies[i];
-        std::vector<uint32_t> sinewave;
+    for (int i = 0; i < 36; i++) {
+        uint16_t T = 22000 / frequencies[i];
+        std::vector<uint16_t> sinewave;
         sinewave.reserve(T); // Reserve memory for efficiency
         for (int j = 0; j < T; j++) {
             sinewave.push_back((sin(2 * M_PI * j / T) + 1) * half_range);
@@ -24,16 +24,16 @@ std::vector<std::vector<uint32_t>> sinewave() {
     return sinewave_lut;
 }
 
-std::vector<std::vector<uint32_t>> sawtooth1() {
-    const double max_range = pow(2, 12) - 1; // Half of the maximum value of uint32_t
-    const uint32_t frequencies[12] = {262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494};
-    std::vector<std::vector<uint32_t>> sawtooth_lut;
-    std::vector<uint32_t> zero_vec;
+std::vector<std::vector<uint16_t>> sawtooth1() {
+    const double max_range = pow(2, 12) - 1; // Maximum value of uint16_t
+    const uint16_t frequencies[36] = {130, 138, 147, 155, 165, 174, 185, 196, 207, 220, 233, 247, 262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523, 554, 587, 622, 659, 698, 740, 784, 830, 880, 932, 988};
+    std::vector<std::vector<uint16_t>> sawtooth_lut;
+    std::vector<uint16_t> zero_vec;
     zero_vec.push_back(0);
     sawtooth_lut.push_back(zero_vec);
-    for (int i = 0; i < 12; i++) {
+    for (int i = 0; i < 36; i++) {
         double T = 22000 / frequencies[i];
-        std::vector<uint32_t> sawtooth;
+        std::vector<uint16_t> sawtooth;
         sawtooth.reserve(T); // Reserve memory for efficiency
         for (int j = 0; j < T; j++) {
             sawtooth.push_back((j/T) * max_range);
@@ -43,20 +43,19 @@ std::vector<std::vector<uint32_t>> sawtooth1() {
     return sawtooth_lut;
 }
 
-std::vector<std::vector<uint32_t>> piano1() {
-    const double half_range = 2047.25; // Half of the maximum value of uint32_t
-    const uint32_t frequencies[12] = {262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494};
-    std::vector<std::vector<uint32_t>> piano_lut;
-    std::vector<uint32_t> zero_vec;
+std::vector<std::vector<uint16_t>> piano1() {
+    const uint16_t frequencies[36] = {130, 138, 147, 155, 165, 174, 185, 196, 207, 220, 233, 247, 262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523, 554, 587, 622, 659, 698, 740, 784, 830, 880, 932, 988};
+    std::vector<std::vector<uint16_t>> piano_lut;
+    std::vector<uint16_t> zero_vec;
     double tone;
     zero_vec.push_back(0);
     piano_lut.push_back(zero_vec);
-    for (int i = 0; i < 12; i++) {
-        uint32_t T = 22000 / frequencies[i];
-        std::vector<uint32_t> pianowave;
+    for (int i = 0; i < 36; i++) {
+        uint16_t T = 22000 / frequencies[i];
+        std::vector<uint16_t> pianowave;
         pianowave.reserve(T); // Reserve memory for efficiency
         for (int j = 0; j < T; j++) {
-            tone = (0.6 * sin(2*M_PI*j/T) + 0.4 * sin(4*M_PI*j/T) + pow((0.6 * sin(2*M_PI*j/T) + 0.4 * sin(4*M_PI*j/T)), 3)) * 2650;
+            tone = (0.6 * sin(2*M_PI*j/T) + 0.4 * sin(4*M_PI*j/T) + pow((0.6 * sin(2*M_PI*j/T) + 0.4 * sin(4*M_PI*j/T)), 3)) * 2688;
             pianowave.push_back(tone);
         }
         piano_lut.push_back(pianowave);
@@ -64,21 +63,42 @@ std::vector<std::vector<uint32_t>> piano1() {
     return piano_lut;
 }
 
-std::vector<std::vector<uint32_t>> sawtooth2() {
-    const double max_range = pow(2, 12) - 1; // Half of the maximum value of uint32_t
-    const uint32_t frequencies[12] = {262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494};
-    std::vector<std::vector<uint32_t>> sawtooth_lut;
-    std::vector<uint32_t> zero_vec;
+std::vector<std::vector<uint16_t>> base1() {
+    const double half_range = pow(2, 12) - 1; // Half of the maximum value of uint16_t
+    const uint16_t frequencies[12] = {262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494};
+    std::vector<std::vector<uint16_t>> base_lut;
+    std::vector<uint16_t> zero_vec;
     zero_vec.push_back(0);
-    sawtooth_lut.push_back(zero_vec);
+    base_lut.push_back(zero_vec);
     for (int i = 0; i < 12; i++) {
         double T = 22000 / frequencies[i];
-        std::vector<uint32_t> sawtooth;
-        sawtooth.reserve(T); // Reserve memory for efficiency
-        for (int j = 0; j < T; j++) {
-            sawtooth.push_back(((T - j)/T) * max_range);
+        std::vector<uint16_t> base;
+        base.reserve(T); // Reserve memory for efficiency
+        for (int j = 0; j < T * 5; j++) {
+            base.push_back(sin(j * exp(-j/(T * 0.7)) + 1) * half_range);
         }
-        sawtooth_lut.push_back(sawtooth);
+        base_lut.push_back(base);
+        // std::cout << i << std::endl;
     }
-    return sawtooth_lut;
+    return base_lut;
+}
+
+std::vector<std::vector<uint16_t>> base2() {
+    const double max_range = pow(2, 12) - 1; // Half of the maximum value of uint16_t
+    const uint16_t frequencies[12] = {262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494};
+    std::vector<std::vector<uint16_t>> base_lut;
+    std::vector<uint16_t> zero_vec;
+    zero_vec.push_back(0);
+    base_lut.push_back(zero_vec);
+    for (int i = 0; i < 12; i++) {
+        double T = 22000 / frequencies[i];
+        std::vector<uint16_t> base;
+        base.reserve(T); // Reserve memory for efficiency
+        for (int j = 0; j < T * 5; j++) {
+            base.push_back(sin(j * exp(-j/(T * 5))) * 1024);
+        }
+        base_lut.push_back(base);
+        // std::cout << i << std::endl;
+    }
+    return base_lut;
 }
